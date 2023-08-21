341a3cfb2ff8ba8c19e342f352cf3364cd957276
Alma <almagdeveloper@gmail.com>
2023-08-21 17:36:15 -0300

    modificacion de read cities

--- controllers/cities/read.js
+++ controllers/cities/read.js
@@ -2,22 +2,16 @@ import City from "../../models/City.js";
 
 export default async(req, res)=>{
     try{ 
-        let allCities = await City.find()
-        return res.status(200).json({
+        let objetoDeBusqueda = {}
+        let objetoDeOrdenamiento = {}
+        //let allCities = await City.find()
+       /*  return res.status(200).json({
             success: true,
             message:'city found',
             response: allCities
-        })
-    } catch (error) {
-        return res.status(400).json({
-            success:false,
-            message:'not found',
-            response: null
-        })
-    }
-}
-
-       {/* if (req.query.admin_id) {
+        }) */
+        
+        if (req.query.admin_id) {
             objetoDeBusqueda.admin_id = req.query.admin_id
         }
         if (req.query.city) {
@@ -32,16 +26,24 @@ export default async(req, res)=>{
             .sort(objetoDeOrdenamiento)
 
         if (allCities.length>0) {
-            return res.status(200).josn({
+            return res.status(200).json({
                 success: true,
                 message: 'cities found',
                 response: allCities
             })
-
+            
         } else {
             return res.status(400).json({
                 success: false,
                 message: 'not found',
                 response: null
             })
-        }*/}
+        }
+    } catch (error) {
+        return res.status(400).json({
+            success:false,
+            message:'not found',
+            response: null
+        })
+    }
+}

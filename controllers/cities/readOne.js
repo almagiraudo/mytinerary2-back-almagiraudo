bc118eef063310caffc90acb615393adf631dd7a
Alma <almagdeveloper@gmail.com>
2023-08-29 21:59:47 -0300

    modificacion de read itineraries

--- controllers/cities/readOne.js
+++ controllers/cities/readOne.js
@@ -4,7 +4,7 @@ export default async(req, res)=>{
     try{
         let oneCity = await City
         .findOne({_id:req.params._id})
-        // .select("country city photo")
+        //.select("country city photo")
          console.log(oneCity)
         return res.status(200).json({
             success:true,
--- controllers/itineraries/read.js
+++ controllers/itineraries/read.js
@@ -2,12 +2,14 @@ import Itinerary from "../../models/Itinerary.js";
 
 export default async(req, res, next)=>{
     try{
-        let queries = {}
+         let queries = {}
         if(req.query.city_id){
             queries.city_id = req.query.city_id
-        }
+        }console.log(queries) 
+       
         let allItineraries = await Itinerary.find(queries, '-__v -createdAt -updatedAt')
-        .populate('city_id', 'city photo admin_id')
+        .populate('city_id', 'city, photo, admin_id')
+        
         return res.status(200).json({
             success: true,
             message:'itineraries found ',

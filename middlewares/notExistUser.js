55c6881827d4ff5b5e799101ee8f9f1a27b73ee0
Alma <almagdeveloper@gmail.com>
2023-09-06 16:27:49 -0300

    middlewares

---
+++ middlewares/notExistUser.js
@@ -0,0 +1,19 @@
+import User from "../models/User.js";
+
+export default async (req, res, next)=>{
+    try {
+        let one= await User.findOne({mail: req.body.mail}, '-password');
+        if(!one){
+            return res.status(400).json({
+                success: false,
+                message:"user has not been registered",
+                response: null
+            });
+        }else{
+            req.user = one
+            return next()
+        }
+    } catch (error) {
+        return next(error)
+    }
+}

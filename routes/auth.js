eb9b5a6ac011c51ea841e2bf6d67e3e797cf461c
Alma <100444289+almagiraudo@users.noreply.github.com>
2023-09-06 19:09:34 -0300

    logout (#9)

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import passport from "passport";
+import signout from "../controllers/auth/signout.js";
 import register from "../controllers/auth/register.js";
 import notExistUser from "../middlewares/notExistUser.js";
-
+import token from "../controllers/auth/token.js";
 import validator from "../middlewares/validator.js";
 import registerSchema from "../schemas/register.js";
 import existsUser from "../middlewares/existsUser.js";
@@ -14,5 +16,6 @@ let auth_router = Router()
 
 auth_router.post('/signup',validator(registerSchema),existsUser,validPassword,register)
 auth_router.post('/signin',validator(signinSchema),notExistUser,isPassOk,isValidToken, signIn)
-
+auth_router.post('/token', passport.authenticate('jwt',{session:false}), isValidToken,token)
+auth_router.post('/signout',passport.authenticate('jwt',{session:false}),signout)
 export default auth_router;

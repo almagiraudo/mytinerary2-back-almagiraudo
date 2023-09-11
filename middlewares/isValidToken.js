151973d4a64597ae824b2aa7f23eb1bfd3f9223f
Alma <almagdeveloper@gmail.com>
2023-09-11 18:49:08 -0300

    entrega final :D

--- middlewares/isValidToken.js
+++ middlewares/isValidToken.js
@@ -5,7 +5,7 @@ export default(req,res,next)=>{
         let token = jwt.sign(
             {mail:req.user.mail},//objeto a convertir en token
             process.env.SECRET_KEY,          //PALABRA QUE SIRVE DE LLAVE PARA TOKENIZAR
-            {expiresIn: 60*60*24*7}
+            {expiresIn: 60*60*24*1}
         )
         req.token= token
         console.log(req.token);
